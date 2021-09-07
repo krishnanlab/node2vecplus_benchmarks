76ac09c0763909ff65b4496b00c78be00aeea0c1
Remy <liurenmi@msu.edu>
2021-09-06 23:25:33 -0400

    Remove temporary test script

--- script/gnn/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash --login
-
-source ~/.bashrc
-conda activate node2vecplus-bench
-
-python gnn.py --network_path ../../data/networks/ppi/STRING.npz --dataset_path ../../data/labels/gene_classification/STRING_KEGGBP_label_split.npz --epochs 100 --eval_steps 10
-
