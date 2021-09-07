acb7bbce2b7a75c90db895400705f2a2d38c197a
Remy <liurenmi@msu.edu>
2021-09-07 19:37:48 -0400

    Move embed function to util; move common variables to common_var; fix directory check

--- script/eval_gene_classification_n2v.py
+++ script/eval_gene_classification_n2v.py
@@ -4,39 +4,30 @@ from time import time
 
 import numpy as np
 import pandas as pd
-import numba
-NUM_THREADS = 4
-numba.set_num_threads(NUM_THREADS)
-
-from pecanpy import node2vec
-from gensim.models import Word2Vec
 
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import average_precision_score
 
 from util import *
 
 
-DATA_DIR = "../data"
-RESULT_DIR = "../result"
 OUTPUT_DIR = f"{RESULT_DIR}/gene_classification_n2v"
 NETWORK_DIR = f"{DATA_DIR}/networks/ppi"
 LABEL_DIR = f"{DATA_DIR}/labels/gene_classification"
 
 # check if output directory exist and create it not
-if not os.path.isdir(RESULT_DIR):
+try:
     os.makedirs(RESULT_DIR)
-if not os.path.isdir(OUTPUT_DIR):
+except FileExistsError:
+    pass
+try:
     os.makedirs(OUTPUT_DIR)
+except FileExistsError:
+    pass
 
 DATASET_LIST = ['GOBP', 'KEGGBP', 'DisGeNet']
 
 ###DEFAULT HYPER PARAMS###
 HPARAM_DIM = 128
-HPARAM_NUMWALKS = 10
-HPARAM_WALKLENGTH = 80
-HPARAM_WINDOW = 10
-HPARAM_EPOCHS = 1
 ##########################
 
 
@@ -66,25 +57,6 @@ def parse_args():
     print(args)
 
     return args
-
-
-def _embed(network_fp, extend, p, q):
-    # initialize DenseOTF graph
-    adj_mat, IDs = np.load(network_fp).values()
-    g = node2vec.DenseOTF(p=p, q=q, workers=NUM_THREADS, verbose=False, extend=extend)
-    g.from_mat(adj_mat, IDs)
-
-    # simulate random walks and genearte embedings
-    walks = g.simulate_walks(num_walks=HPARAM_NUMWALKS, walk_length=HPARAM_WALKLENGTH)
-    w2v = Word2Vec(walks, vector_size=HPARAM_DIM, window=HPARAM_WINDOW,
-                   min_count=0, sg=1, workers=NUM_THREADS, epochs=HPARAM_EPOCHS)
-
-    # sort embeddings by IDs
-    IDmap = {j:i for i,j in enumerate(w2v.wv.index_to_key)}
-    idx_ary = [IDmap[i] for i in IDs]
-    X_emd = w2v.wv.vectors[idx_ary]
-
-    return X_emd, IDs
 
 
 def _evaluate(X_emd, IDs, label_fp, random_state):
@@ -129,7 +101,7 @@ def evaluate(args):
 
     # generate embeddings and report time usage
     t = time()
-    X_emd, IDs = _embed(network_fp, extend, p, q)
+    X_emd, IDs = embed(network_fp, HPARAM_DIM, extend, p, q)
     t = time() - t
     print(f"Took {int(t/3600):02d}:{int(t/60):02d}:{t%60:05.02f} to generate embeddings using {method}")
 
