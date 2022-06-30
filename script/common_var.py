38f534d26b7e8b7753a965399d0ea461d75bb2b8
Remy <liurenmi@msu.edu>
2022-06-30 07:40:56 -0400

    Update labelsets
    - Use HumanBase + GTEx genes, replace GIANT (former version of HumanBase)
    - Use preprocessed GOBP and DisGeNet from GenePlexus

--- script/common_var.py
+++ script/common_var.py
@@ -1,5 +1,14 @@
-DATA_DIR = "../data"
-RESULT_DIR = "../result"
+import pathlib
+
+HOME_DIR = pathlib.Path(__file__).absolute().parents[1]
+DATA_DIR = HOME_DIR / "data"
+RESULT_DIR = HOME_DIR / "result"
+
+LABEL_PATH_DICT = {
+    "DisGeNet": "/mnt/research/compbio/krishnanlab/data/disease-gene_annotations/disgenet/disgenet_disease-genes_prop.gsea-min10-max600-ovlppt7-jacpt5.nonred.gmt",
+    "GOBP": "/mnt/research/compbio/krishnanlab/data/functional_annotations/go/go_bp-genes_exp-ec_prop_pos-slim.gsea-min10-max200-ovlppt7-jacpt5.nonred.gmt",
+}
+PUBMED_COUNT_PATH = "/mnt/research/compbio/krishnanlab/data/pubmed/gene2pubmed_human_gene-counts.txt"
 
 REPETITION = 10
 
@@ -7,4 +16,3 @@ W2V_NUMWALKS = 10
 W2V_WALKLENGTH = 80
 W2V_WINDOW = 10
 W2V_EPOCHS = 1
-
---
+++ script/process_labels.py
@@ -0,0 +1,81 @@
+import logging
+import os
+import pathlib
+from typing import Union, List
+
+import numpy as np
+from NLEval.label import filters
+from NLEval.label import LabelsetCollection
+from NLEval.label.split import RatioPartition
+
+from util import config_logger
+from common_var import DATA_DIR, LABEL_PATH_DICT, PUBMED_COUNT_PATH
+
+config_logger()
+logger = logging.getLogger(__name__)
+
+STRING_NETWORK_PATH = DATA_DIR / "networks/ppi/STRING.npz"
+HB_NETWORK_DIR = DATA_DIR / "networks/gtexcoexp"
+GTX_NETWORK_DIR = DATA_DIR / "networks/humanbase"
+HBGTX_NETWORK_PATHS = (
+    os.popen(f"ls {HB_NETWORK_DIR}/*.npz").read().split()
+    + os.popen(f"ls {GTX_NETWORK_DIR}/*.npz").read().split()
+)
+
+MIN_NUM_POS = 10  # minimum number of positives per split
+SPLIT_RATIOS = (0.6, 0.2, 0.2)  # train/val/test split ratios
+
+DATASETS = ["GOBP", "DisGeNet"]
+
+
+def filter_lsc(network_paths: Union[str, List[str]], name: str, dataset: str):
+    """Filter label set collection using network genes.
+
+    Args:
+        network_paths: Path(s) to the network(s).
+        name: Name of the network genes.
+        dataset: Name of the dataset.
+
+    """
+    # Load netowrk genes
+    network_paths = network_paths if isinstance(network_paths, list) else [network_paths]
+    gene_ids = tuple(set.intersection(*[set(np.load(i)["IDs"].tolist()) for i in network_paths]))
+    logger.info(f"Loaded {len(gene_ids):,} number of genes")
+
+    # Apply basic filtering (gene intersection & labelset size)
+    lsc = LabelsetCollection.from_gmt(LABEL_PATH_DICT[dataset])
+    lsc.iapply(filters.EntityExistenceFilter(gene_ids), progress_bar=True)
+    lsc.iapply(filters.LabelsetRangeFilterSize(min_val=50), progress_bar=True)
+    lsc.load_entity_properties(PUBMED_COUNT_PATH, "PubMed Count", 0, int)
+
+    # Apply (study-bias holdout) split size filtering
+    splitter = RatioPartition(*SPLIT_RATIOS, ascending=False)
+    split_filter = filters.LabelsetRangeFilterSplit(MIN_NUM_POS, splitter, property_name="PubMed Count")
+    lsc.iapply(split_filter, progress_bar=True)
+
+    # Generate label and masks
+    y, masks = lsc.split(splitter, target_ids=gene_ids, property_name="PubMed Count")
+    logger.info(f"{y.sum(0)=}")
+    logger.info(f"{y[masks['test'][:, 0]].sum(0)=}")
+    logger.info(f"{y.shape=}")
+
+    # Save labelset npz
+    np.savez(
+        DATA_DIR / f"labels/gene_classification/{name}_{dataset}_label_split.npz",
+        y=y,
+        train_idx=np.where(masks["train"][:, 0])[0],
+        valid_idx=np.where(masks["val"][:, 0])[0],
+        test_idx=np.where(masks["test"][:, 0])[0],
+        label_ids=lsc.label_ids,
+        gene_ids=gene_ids,
+    )
+
+
+def main():
+    for dataset in DATASETS:
+        filter_lsc(STRING_NETWORK_PATH, "STRING", dataset)
+        filter_lsc(HBGTX_NETWORK_PATHS, "HBGTX", dataset)
+
+
+if __name__ == "__main__":
+    main()
