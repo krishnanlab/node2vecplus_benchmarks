29a6783bf100b25a19ee137e1784932fcc3f2ab9
Remy <liurenmi@msu.edu>
2022-10-21 16:05:55 -0400

    add hyper-parameter tuning mode

--- script/eval_gene_classification_gnn.py
+++ script/eval_gene_classification_gnn.py
@@ -1,6 +1,9 @@
 import argparse
 import itertools
+import os
 import time
+from pprint import pprint
+from typing import Tuple
 
 import numpy as np
 import pandas as pd
@@ -17,28 +20,31 @@ from util import *
 
 
 OUTPUT_DIR = f"{RESULT_DIR}/gene_classification_gnn"
+HP_TUNE_OUTPUT_DIR = f"{RESULT_DIR}/gene_classification_gnn_hp_tune"
 NETWORK_DIR = f"{DATA_DIR}/networks/ppi"
 LABEL_DIR = f"{DATA_DIR}/labels/gene_classification"
 
-check_dirs([RESULT_DIR, OUTPUT_DIR])
+check_dirs([RESULT_DIR, OUTPUT_DIR, HP_TUNE_OUTPUT_DIR])
 
-#####DEFAULT PARAMETERS#####
+########## PARAMETERS ##########
+EPOCHS = 50_000
 EVAL_STEPS = 100
 SCHEDULER_PATIENCE = 500
 
-LR = 0.01
-EPOCHS = 2500
-WEIGHT_DECAY = 0.00001
-
 HPARAM_GCN_DIM = 128
-HPARAM_GCN_LR = 0.01
-HPARAM_GCN_RESIDUAL = True
 HPARAM_GCN_NUM_LAYERS = 5
+HPARAM_GCN_RESIDUAL = True
+HPARAM_GCN_LR = 0.01
+HPARAM_GCN_DROPOUT = 0.1
+HPARAM_GCN_WEIGHT_DECAY = 1e-5
 
 HPARAM_SAGE_DIM = 128
-HPARAM_SAGE_RESIDUAL = False
 HPARAM_SAGE_NUM_LAYERS = 5
-############################
+HPARAM_SAGE_RESIDUAL = False
+HPARAM_SAGE_LR = 0.01
+HPARAM_SAGE_DROPOUT = 0.1
+HPARAM_SAGE_WEIGHT_DECAY = 1e-5
+################################
 
 
 class GNN(nn.Module):
@@ -142,12 +148,23 @@ def parse_args():
     parser.add_argument("--network", required=True, help="Name of the protein protein interaction network")
     parser.add_argument("--dataset", required=True, help="Name of geneset collection")
     parser.add_argument("--device", type=int, default=0, help="Device number indicating which GPU to use, default is 0")
-    parser.add_argument("--nooutput", action="store_true", help="Disable output if specified, and print results to screen")
+    parser.add_argument("--runid", type=int, default=0, help="Run ID, for separating results from different runs")
+    parser.add_argument("--hp_tune", action="store_true", help="Hyper-parameter tuning mode, use parsed params.")
+    parser.add_argument("--nooutput", action="store_true", help="Disable output if specified")
     parser.add_argument("--use_sage", action="store_true", help="Use GraphSAGE instead of GCN, defulat is using GCN")
     parser.add_argument("--test", action="store_true", help="Toggle test mode, run with fewer epochs")
+    parser.add_argument("--dry_run", action="store_true", help="Print model and parameter settings and exit.")
+
+    group_hp = parser.add_argument_group("Hyper-parameters")
+    group_hp.add_argument("--dim", type=int, default=64, help="Convolution layer hidden dimension")
+    group_hp.add_argument("--num_layers", type=int, default=3, help="Number of convolution layers")
+    group_hp.add_argument("--residual", action="store_true", help="Whether or not to add residual connection (skipsum)")
+    group_hp.add_argument("--lr", type=float, default=0.01, help="Learning rate")
+    group_hp.add_argument("--weight_decay", type=float, default=1e-5, help="Weight decay (l2 regularization)")
+    group_hp.add_argument("--dropout", type=float, default=0.1, help="Dropout probability (excluding pre-mlp)")
 
     args = parser.parse_args()
-    print(args)
+    pprint(vars(args))
 
     return args
 
@@ -178,43 +195,86 @@ def load_data(gene_universe, network, dataset, use_sage, device):
     return adj, x, y, train_idx, valid_idx, test_idx, label_ids
 
 
-def main(args):
+def parse_hparams(args: argparse.Namespace) -> Tuple[int, int, bool, float, float, float]:
+    # Load default hyper-parameter settings
+    if args.use_sage:
+        dim = HPARAM_SAGE_DIM
+        num_layers = HPARAM_SAGE_NUM_LAYERS
+        residual = HPARAM_SAGE_RESIDUAL
+        lr = HPARAM_SAGE_LR
+        dropout = HPARAM_SAGE_DROPOUT
+        weight_decay = HPARAM_SAGE_WEIGHT_DECAY
+    else:
+        dim = HPARAM_GCN_DIM
+        num_layers = HPARAM_GCN_NUM_LAYERS
+        residual = HPARAM_GCN_RESIDUAL
+        lr = HPARAM_GCN_LR
+        dropout = HPARAM_GCN_DROPOUT
+        weight_decay = HPARAM_GCN_WEIGHT_DECAY
+
+    # Overwrite hyper-parameters if hp_tune mode is set to True
+    if args.hp_tune:
+        dim = args.dim
+        num_layers = args.num_layers
+        residual = args.residual
+        lr = args.lr
+        dropout = args.dropout
+        weight_decay = args.weight_decay
+
+    print("\nHyper-parameter settings:")
+    print(f"{dim=}\n{num_layers=}\n{residual=}\n{lr=}\n{dropout=}\n{weight_decay=}\n")
+
+    return dim, num_layers, residual, lr, dropout, weight_decay
+
+
+def main():
+    args = parse_args()
     gene_universe = args.gene_universe
     network = args.network
     dataset = args.dataset
     nooutput = args.nooutput
     device = args.device
+    hp_tune = args.hp_tune
+    dry_run = args.dry_run
     use_sage = args.use_sage
+    runid = args.runid
     device = f"cuda:{device}" if torch.cuda.is_available() else "cpu"
     method = "sage" if use_sage else "gcn"
 
+    # Parse hyper parameters and training settings
+    dim, num_layers, residual, lr, dropout, weight_decay = parse_hparams(args)
+    epochs, eval_steps = EPOCHS, EVAL_STEPS
     if args.test:
         print("WARNING: runing in testing mode")
-        EPOCHS = 100
-        EVAL_STEPS = 10
+        epochs = 100
+        eval_steps = 10
+    elif args.hp_tune:
+        epochs = 2500
+    patience = max(1, int(SCHEDULER_PATIENCE // eval_steps))
+
+    # Get output path
+    if not hp_tune:
+        out_path = f"{OUTPUT_DIR}/{network}_{dataset}_{method}.csv"
     else:
-        EPOCHS = 30000
-        EVAL_STEPS = 100
+        hparams_str = f"dim={dim}_num-layers={num_layers}_residual={1 if residual else 0}"
+        hparams_str += f"_lr={lr:.0e}_dropout={dropout}_weight-decay={weight_decay:.0e}"
+        out_path = f"{HP_TUNE_OUTPUT_DIR}/{method}/{hparams_str}/{network}_{dataset}_{runid}/score.csv"
+    os.makedirs(os.path.split(out_path)[0], exist_ok=True)
 
-    # load and constructing data object
+    # Load data
     adj, x, y, train_idx, valid_idx, test_idx, label_ids = load_data(gene_universe, network, dataset, use_sage, device)
-    data = Data(x=x, y=y)
-    data.adj = adj
-    data = data.to(device)
+    data = Data(x=x, y=y, adj=adj).to(device)
 
-    # initialize model and optimizer
-    if use_sage:  # GraphSAGE, use degree as feature
-        model_args = (DenseSAGEConv, x.shape[1], HPARAM_SAGE_DIM, y.shape[1], HPARAM_SAGE_NUM_LAYERS)
-        model_kwargs = {"residual": HPARAM_SAGE_RESIDUAL}
-    else:  # GCN, use constant feature
-        model_args = (DenseGCNConv, x.shape[1], HPARAM_GCN_DIM, y.shape[1], HPARAM_GCN_NUM_LAYERS)
-        model_kwargs = {"residual": HPARAM_GCN_RESIDUAL}
-
-    model = GNN(*model_args, **model_kwargs).to(device)
-    patience = max(1, int(SCHEDULER_PATIENCE // EVAL_STEPS))
+    # Initialize model and optimizer
+    conv_module = DenseSAGEConv if use_sage else DenseGCNConv
+    model = GNN(conv_module, x.shape[1], dim, y.shape[1], num_layers, residual=residual, dropout=dropout).to(device)
     print(f"GNN model:\n{model}")
 
-    optimizer = Adam(model.parameters(), lr=LR, weight_decay=WEIGHT_DECAY)
+    if dry_run:
+        print(f"Results will be saved to {out_path}")
+        exit(0)
+
+    optimizer = Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
     scheduler = ReduceLROnPlateau(optimizer, mode="max", factor=0.5, patience=patience, verbose=True)
 
     # Train model and record best performance
@@ -222,10 +282,10 @@ def main(args):
     best_valid_score = 0
     best_results = None
     tic = time.perf_counter()
-    for epoch in range(1, 1 + EPOCHS):
+    for epoch in range(1, 1 + epochs):
         loss = train(model, data, train_idx, optimizer)
 
-        if epoch % EVAL_STEPS == 0:
+        if epoch % eval_steps == 0:
             results = test(model, data, train_idx, valid_idx, test_idx)
             train_score, valid_score, test_score = [sum(score_lst) / len(score_lst)
                                                     for score_lst in results]
@@ -240,7 +300,7 @@ def main(args):
                 f"Epoch: {epoch:4d}, Loss: {loss:.4f}, "
                 f"Train: {train_score:.4f}, Valid: {valid_score:.4f}, "
                 f"Test: {test_score:.4f}, Best epoch so far: {best_epoch:4d} "
-                f"({elapsed / EVAL_STEPS / 60 * 1000:.2f} min/kEpoch)",
+                f"({elapsed / eval_steps / 60 * 1000:.2f} min/kEpoch)",
             )
             tic = time.perf_counter()
 
@@ -251,12 +311,11 @@ def main(args):
     result_df["Dataset"], result_df["Network"], result_df["Method"] = dataset, network, method
 
     # Save or print results
-    if nooutput:
-        print(result_df)
-    else:
-        result_df.to_csv(f"{OUTPUT_DIR}/{network}_{dataset}_{method}.csv", index=False)
+    print(result_df)
+    if not nooutput:
+        result_df.to_csv(out_path, index=False)
+        print(f"Results saved to {out_path}")
 
 
 if __name__ == "__main__":
-    args = parse_args()
-    main(args)
+    main()
